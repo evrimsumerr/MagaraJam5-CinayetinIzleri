ee0a847b0013a064340ea97d66557123915e5bcc
Bekir Evrim Sümer <bekirevrimsumer@gmail.com>
2022-09-15 22:51:16 +0300

    Generic Singleton

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : GenericSingleton<GameManager>
+{
+    public bool IsStarted;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : GenericSingleton<UIManager>
+{
+    void Start()
+    {
+        if (GameManager.Instance.IsStarted)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
