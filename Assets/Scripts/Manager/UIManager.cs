b2412b34c370161aab04c1480df524781e6254b8
Harun Aşık <harun.esad@gmail.com>
2022-09-16 16:14:19 +0300

    tasklist

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : GenericSingleton<UIManager>
 {
-    void Start()
+    [SerializeField] TextMeshProUGUI openText;
+    [SerializeField] TextMeshProUGUI closeText;
+    [SerializeField] Image questsBg;
+    bool openState;
+    bool closeState;
+    bool questsBgState;
+    public void OpenQuests()
     {
-        if (GameManager.Instance.IsStarted)
-        {
-
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        openState = openText.gameObject.activeSelf;
+        closeState = closeText.gameObject.activeSelf;
+        questsBgState = questsBg.gameObject.activeSelf;
+        closeText.gameObject.SetActive(!closeState);
+        openText.gameObject.SetActive(!openState);
+        questsBg.gameObject.SetActive(!questsBgState);
     }
 }
